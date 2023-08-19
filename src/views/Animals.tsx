8332d1b1ffefcc5440a8cddca86b55efd229b783
Veronica <veronica.bergstrom@medieinstitutet.se>
2023-08-19 11:56:20 +0200

    added css for animals view

--- src/views/Animals.tsx
+++ src/views/Animals.tsx
@@ -1,20 +1,39 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { IAnimalsResponse } from "../models/IAnimalsResponse";
+import "../App.css";
+import { Header } from "../components/Header";
+
+// Defaults to the Classic family, Solid style
 
 export const Animals = () => {
   const data = useLoaderData() as IAnimalsResponse;
 
   return (
-    <ul>
-      {data.map((animal) => {
-        return (
-          <>
-            <img src={animal.imageUrl} alt={animal.latinName} />
-            <li key={animal.id}>{animal.name}</li>
-            <p>{animal.shortDescription}</p>
-          </>
-        );
-      })}
-    </ul>
+    <>
+      <Header />
+      <ul className="animals-list">
+        {data.map((animal) => {
+          return (
+            <li key={animal.id}>
+              <Link to={`animal/${animal.id}`}>
+                <img
+                  className="image"
+                  src={animal.imageUrl}
+                  alt={animal.latinName}
+                />
+              </Link>
+              <div className="animals-text">
+                <h2>{animal.name}</h2>
+                <p>{animal.shortDescription}</p>
+                <Link to={`animal/${animal.id}`}>
+                  <h3 className="animals-hungry">{animal.name} är hungrig!!</h3>
+                </Link>
+                <span></span>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    </>
   );
 };
